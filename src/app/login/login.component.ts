069517813aee89d3f07ae52452636cb941b16ccf
SyChester <Chester_Nevan_Sy@manulife.ca>
2022-10-15 20:00:28 +0800

    Added methods for authentication

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import profile from '../../../database/profile.json';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
+
+  usernameVal = '';
+  passwordVal = '';
 
   ngOnInit(): void {
+  }
+
+  usernameKey(value: string) {
+    this.usernameVal = value;
+  }
+
+  passwordKey(value: string) {
+    this.passwordVal = value;
+  }
+
+  onLogin() {
+    if (!this.usernameVal || !this.passwordVal) {
+      throw new Error('Username or Password is Empty');
+    }
+
+    const userExists = profile.find(obj => obj.username === this.usernameVal && obj.password === this.passwordVal);
+
+    if (userExists) {
+      this.router.navigate([`capabilities`]);
+    } else {
+      console.log('MALI');
+    }
   }
 
 }
