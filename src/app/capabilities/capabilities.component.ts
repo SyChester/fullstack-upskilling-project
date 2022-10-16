54833ff4f38a40c599da82341ee37f24460e41eb
SyChester <Chester_Nevan_Sy@manulife.ca>
2022-10-16 20:53:54 +0800

    Added logic for capabilities component

--- src/app/capabilities/capabilities.component.ts
+++ src/app/capabilities/capabilities.component.ts
@@ -1,4 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+
+export interface TableElement {
+  name: string;
+  workday: string;
+  title: string;
+  project: string;
+  date: string;
+}
+
+const ELEMENT_DATA: TableElement[] = [
+  {name: 'Jeniffer', workday: '234545', title: 'Assoc', project: 'GN', date: '2022-12-12'},
+  {name: 'Alisson', workday: '934234', title: 'Intermediate', project: 'TripX', date: '2022-10-02'},
+  {name: 'Bob', workday: '935235', title: 'Senior', project: 'Bank', date: '2021-12-11'},
+  {name: 'Jeniffer', workday: '234545', title: 'Assoc', project: 'GN', date: '2022-12-12'},
+  {name: 'Alisson', workday: '934234', title: 'Intermediate', project: 'TripX', date: '2022-10-02'},
+  {name: 'Bob', workday: '935235', title: 'Senior', project: 'Bank', date: '2021-12-11'},
+  {name: 'Jeniffer', workday: '234545', title: 'Assoc', project: 'GN', date: '2022-12-12'},
+  {name: 'Alisson', workday: '934234', title: 'Intermediate', project: 'TripX', date: '2022-10-02'},
+  {name: 'Bob', workday: '935235', title: 'Senior', project: 'Bank', date: '2021-12-11'},
+  {name: 'Jeniffer', workday: '234545', title: 'Assoc', project: 'GN', date: '2022-12-12'},
+  {name: 'Alisson', workday: '934234', title: 'Intermediate', project: 'TripX', date: '2022-10-02'},
+  {name: 'Bob', workday: '935235', title: 'Senior', project: 'Bank', date: '2021-12-11'},
+  {name: 'Jeniffer', workday: '234545', title: 'Assoc', project: 'GN', date: '2022-12-12'},
+  {name: 'Alisson', workday: '934234', title: 'Intermediate', project: 'TripX', date: '2022-10-02'},
+  {name: 'Bob', workday: '935235', title: 'Senior', project: 'Bank', date: '2021-12-11'}
+];
 
 @Component({
   selector: 'app-capabilities',
@@ -6,10 +36,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./capabilities.component.css']
 })
 export class CapabilitiesComponent implements OnInit {
+  firstName = '';
+  lastName = '';
+  resourceId = '';
+  search = '';
+  displayedColumns = ['name', 'workday', 'title', 'project', 'date'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
 
-  constructor() { }
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+  constructor(private router: Router) {
+    const navigation = this.router.getCurrentNavigation();
+    const state = navigation!.extras.state as {
+      firstName: string,
+      lastName: string,
+      resourceId: string,
+    };
+
+    this.firstName = state.firstName;
+    this.lastName = state.lastName;
+    this.resourceId = state.resourceId;
+  }
 
   ngOnInit(): void {
+    //balak ko ilagay dito deets ni user. Para pagclick nung expandable, lalabas sya agad
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  filter(value: string) {
+    this.dataSource.filter = value.trim().toLowerCase();
   }
 
 }
