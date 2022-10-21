5c6c464bdb877038ea6bc0c580b605dd83674d98
SyChester <Chester_Nevan_Sy@manulife.ca>
2022-10-21 13:08:59 +0800

    Added capabilities dto

---
+++ src/app/capabilities/dto/capabilities.dto.ts
@@ -0,0 +1,16 @@
+export interface UserDetails {
+  firstName: string;
+  lastName: string;
+  workday: string;
+  title: string;
+  project: string;
+  date_hired: string;
+}
+
+export interface TableElement {
+  name: string;
+  workday: string;
+  title: string;
+  project: string;
+  date: string;
+}
